cbbfe472fa9f9023aadcd0de0ac1f8ff4fa7c2d7
Rowell Heria <owieh@me.com>
2017-02-23 13:16:48 +0000

    updated hexo type definitions

--- src/hexo.d.ts
+++ src/hexo.d.ts
@@ -1,1 +1,16 @@
-declare var hexo;
+declare interface IHexoTagOptions {
+    async: boolean;
+    ends: boolean;
+}
+
+declare interface IHexoTag {
+    register: (name: string, callback: (args: string[], content: string) => string, options: IHexoTagOptions) => void;
+}
+declare interface IHexoExtend {
+    tag: IHexoTag;
+}
+declare interface IHexo {
+    extend: IHexoExtend;
+}
+
+declare var hexo: IHexo;
